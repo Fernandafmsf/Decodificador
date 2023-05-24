09c2f9eec50e4c0d1d355fbcab879ee9f95034bf
Fernandafmsf <fernandafernandesauto@gmail.com>
2023-05-24 17:36:39 -0300

    teste

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 
-const formulario = document.querySelector("form");
+const textArea = document.querySelector(".entrada-texto");
 const resposta = document.querySelector(".inserir-mensagem");
 
-function criptografar(){
 
-  let text = formulario.mensagem.value;
+function btnEncriptar(){
+  criptografar(textArea.value);
+ // resposta.value = textoEncriptado;
+  textArea.value = " ";
+}
 
-  
-  const newText = text
-  .replace(/o/g, "ober")
-  .replace(/a/g, "ai")
-  .replace(/e/g, "enter")
-  .replace(/i/g, "imes")
-  .replace(/u/g, "ufat");
-  
+function criptografar(stringEncriptada){
+ 
 
-  alert(joinText);
+  let matrizCodigo = [["e", "enter"], ["i", "inter"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]; 
+  stringEncriptada = stringEncriptada.toLowerCase();
 
+  for (let i = 0; i < matrizCodigo.length; i++){
+    if (text.includes(matrizCodigo[i][0])){
 
+      text=text.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+    }
+  }
+  console.log(stringEncriptada) ;
+
+  return stringEncriptada
+
+}
+
+function descriptografar(){
+  var text = formulario.mensagem.value;
+
+  let matrizCodigo = [["e", "enter"], ["i", "inter"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]; 
+  text = text.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++){
+    if (text.includes(matrizCodigo[i][1])){
+
+      text=text.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+    }
+  }
+
+  alert(text) ;
 
 }
 
